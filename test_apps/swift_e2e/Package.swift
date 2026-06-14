a382c0ad6c18827960f56f860aa7c25e666efa9e
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-06-14 20:01:29 +0200

    chore(release): v1.9.0-rc.47

--- test_apps/swift_e2e/Package.swift
+++ test_apps/swift_e2e/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .iOS(.v16),
     ],
     dependencies: [
-        .package(url: "https://github.com/kreuzberg-dev/tree-sitter-language-pack", from: "1.9.0-rc.46"),
+        .package(url: "https://github.com/kreuzberg-dev/tree-sitter-language-pack", from: "1.9.0-rc.47"),
     ],
     targets: [
         .testTarget(
